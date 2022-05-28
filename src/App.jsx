bdb8c3d8babe9e22c6bad1c735ab1916458e6dd4
shubhamdixit863 <shubhamdixit863@gmail.com>
2022-05-28 21:55:21 +0530

    react ui and usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,59 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+// What is react ?
+
+//React is a js library used to build ui appplications
+// React is used to create single page applications
+
+// it enhances the user experience /utility and the efficiency of the fronetend app
+
+import Home from "./components/Home";   // i am importing a component --->
+
+import Footer from "./components/Footer"
+
+import Header from './components/Header';
+import Signup from './components/Signup';
+import { Login } from './components/Login';
+
 function App() {
-  let  [value, setValue] = useState(0) // hook -->
-  let  [name, setName] = useState("vijay") // hook -->
-  let [input,setInput]=useState("");
-  console.log(value);
+  const [state,setState]=useState("");
 
-  function increment(){
-    value++;
-    setValue(value);
-
-  }
-
-  function decrement(){
-    value--;
-    setValue(value);
-
-  }
-
-  function makeUpper(){
-    name=name.toUpperCase();
-
-    setName(name) ;// this is ver important
-
-  }
-
-  function getInputValue(event){
-
-    setInput(event.target.value);
-
-  }
-
+  
 
   return (
-    <div className="App">
-      <h1>{value}</h1>
-      <h1>{input}</h1>
 
-    
-     <button onClick={increment}>Increment</button>
+    <div className='container-fluid'>
 
-     <button onClick={decrement}>Decrement</button>
+     <Header/>
 
-     <input type="text"  onChange={getInputValue}/>
+     <div className='row'  style={{marginTop:"120px"}}>
 
-     <button onClick={makeUpper}>Make Upper Case</button>
+       <div className='col-md-4 offset-2'>
+         <Signup/>
+
+       </div>
+
+       <div className='col-md-4 offset-1'>
       
+      <Login/>
+
+       </div>
+
+     </div>
+
+
+
+      <Footer/>
+
     </div>
+  )
 
 
       
 
-  )
+  
 }
 
 export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+// Is called as component
+import React from 'react'
+
+const Home = () => {
+
+    // In return statements you have to write the html
+  return (
+    <div className='row'>
+      <div className='col-md-6 offset-3' style={{marginTop:"120px"}}>
+
+        <img src="https://media.istockphoto.com/photos/playful-happy-smiling-pet-dog-running-in-the-grass-picture-id1320018473?b=1&k=20&m=1320018473&s=170667a&w=0&h=Q-U9yI4JjCJYSAzXZwpnM4HuaXPzo4K-vBsgO7lanyo=" alt="" srcset="" />
+      
+
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default Home
